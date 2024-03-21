13f682349a17a6c3932b858e580c59ba00520ab4
Nandini <nandinigupta63696@gmail.com>
2024-03-21 18:40:40 +0530

    final commit

--- src/Components/Community2-section/components/Community2.jsx
+++ src/Components/Community2-section/components/Community2.jsx
@@ -6,16 +6,15 @@ const PhotoSlider = () => {
     <div className='d-flex flex-column gap-5'>
       <div className='d-flex flex-column align-items-center mb-4'>
           <img src={logo} alt='community2' width={10} className=''/>
-          <div className='d-flex flex-column align-items-center gap-4'>
+          <div className='d-flex flex-column align-items-center justify-center gap-4'>
             <h1 className='neuton-regular  '>Community</h1>
             <h4>Don’t network. Build an inner circle.</h4>
             <p>Over 1 million messages were exchanged in 2023 so far.</p>
           </div>
       </div>
-      
       <div className='video-container'>
         <video autoPlay loop muted className="video-controller"  width="100%" height="100%">
-          <source src="https://dtoz3qm0nr91u.cloudfront.net/community.mp4" type='video/mp4'/>
+          <source src="https://dtoz3qm0nr91u.cloudfront.net/community.mp4" type='video/mp4' className="videocommunity"/>
         </video>
         <div className='video-container-overlay'></div>
       </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,9 @@
   import './Home.css'
   import Threeoutcomes from './Components/ThreeOutcomes/Threeoutcomes'
   import Threepillors from './Components/ThreePillors/Threepillors'
-  //import Community2 from './Components/Community2-section/components/Community2'
-  //import Member from './Components/Community2-section/components/Member'
-  //import PhotoSlider from './Components/Community2-section/components/Community2'
+  import Community2 from './Components/Community2-section/components/Community2'
+  import Member from './Components/Community2-section/components/Member'
+  import PhotoSlider from './Components/Community2-section/components/Community2'
   import AnnualRetreatSection from './Components/annual-retreat-section/components/AnnualRetreatSection'
   import ExHeadTestimonialSection from './Components/annual-retreat-section/components/ExHeadTestimonialSection'
   import App from './Components/task/App'
@@ -19,14 +19,17 @@
             <button className='btn1'>Become a Member</button>
             <h3>Join leaders at marquee internet companies</h3>
             <img src="https://assets-global.website-files.com/63a49822ee2304acc9455a42/65842ad7fd3d64853ccdf1a7_homapge_logos%20(4)-p-1080.png" className="Homeimg"/>
-            </div>        
-           <App/>
+            </div>
+            <App/>
             
          <Threepillors/>
-         
+         <Community2/>
+            <Member/>
         <Threeoutcomes/>
         <AnnualRetreatSection/>
         <ExHeadTestimonialSection/>
+            
+
             </>
       )
   }
